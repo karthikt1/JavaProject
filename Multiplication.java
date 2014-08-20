dfbf60ad2c82517ac2a3410cc6fa664538007b4a
Karthik <karthikturaga@yahoo.com>
2014-08-20 15:36:00 -0700

    New Java class file

---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+	class Multiplication
+{
+	int a=10, b=20;
+	public void multiplication()
+	{
+		System.out.println("Multiplication of A and B is"+"="+(a*b));
+	}
+	public static void main(String[] args)
+	{
+		Multiplication m= new Multiplication();
+		m.multiplication();
+	}
+}
